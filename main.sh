4041536a25c906a2896220e529ea2d878f57ad8e
Chidananda Sahoo <chidanandsahoo8@gmail.com>
2024-06-09 09:10:18 +0530

    beautify

--- main.sh
+++ main.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
 {
-    echo "################ Fetch Free Disk Space `date` ##################"
+    echo "################ System Monitor v1.0 ### Date: `date` ##################"
+    echo "================ Fetch Free Disk Space=================="
     # Collect mounted free disk space
     df -h
 
     echo ""
-    echo "################ Running Processes ##################"
+    echo "================ Running Processes ================"
     # Collect running processes
     ps aux | grep fis001s
 
     echo ""
-    echo "################ Current CPU Usage ##################"
+    echo "================ Current CPU Usage ================"
     # Collect current CPU usage
     top -n 1 -b
 
     echo ""
-    echo "################ Free and Used Memory ##################"
+    echo "================ Free and Used Memory ================"
     # Collect free and used memory
     free -m
 
     echo ""
-    echo "################ RPM Packages Installed ##################"
+    echo "================ RPM Packages Installed ================"
     # List RPM packages installed
     rpm -qa
 
     echo ""
-    echo "################ System Information ##################"
+    echo "================ System Information ================"
     # Print system information
     uname -a
+
+    echo "================ END OF REPORT ================="
 } > system_info.txt
